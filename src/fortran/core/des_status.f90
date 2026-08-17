787e0ad788e6969d0d01fa70fa9f6f26dcc7df46
Fatih KAVAK <83064648+kavakfatih@users.noreply.github.com>
2026-08-17 23:04:20 +0300

    Minimal Newton solver durum kodlarını ekle

--- src/fortran/core/des_status.f90
+++ src/fortran/core/des_status.f90
@@ -10,4 +10,7 @@ module des_status
   integer, parameter, public :: DES_ERROR_INVALID_ELEMENT_JACOBIAN = -200
   integer, parameter, public :: DES_ERROR_MATERIAL_POINT = -201
   integer, parameter, public :: DES_ERROR_INVALID_CONNECTIVITY = -202
+  integer, parameter, public :: DES_ERROR_INVALID_CONSTRAINT = -300
+  integer, parameter, public :: DES_ERROR_LINEAR_SOLVE = -301
+  integer, parameter, public :: DES_ERROR_NEWTON_DID_NOT_CONVERGE = -302
 end module des_status
